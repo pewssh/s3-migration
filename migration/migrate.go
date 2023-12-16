0919caa0e80462e7d0c5173802bb31b5ce3c3123
peterlimg <54137706+peterlimg@users.noreply.github.com>
2023-12-16 16:47:48 +1100

    Merge pull request #113 from 0chain/fix/chunk-size (#114)
    * README.md changes
    
    --added table of contents
    -- zus overview
    -- added sample migration command syntax
    -- added build instructions
    -- corrected typos
    
    * update
    macos runner
    
    * kishan dhakan suggestions
    
    * Update gosdk.yml: update create-pr workflow
    
    * remove ./build_mocks.sh
    
    * limit range
    
    * comment out build-windows and build-macos
    
    * disable macos windows builds
    
    * upgrade GoSDK to sprint-1.11
    
    * fix chunk size
    
    ---------
    
    Co-authored-by: Harshit Mehndiratta <65766301+hm90121@users.noreply.github.com>
    Co-authored-by: Manohar Reddy <b.manu199@gmail.com>
    Co-authored-by: Kishan Dhakan <42718091+Kishan-Dhakan@users.noreply.github.com>
    Co-authored-by: Yury <yuderbasov@gmail.com>
    Co-authored-by: Hitenjain14 <hitenjain146@gmail.com>
    Co-authored-by: service-0chain <service-0chain@users.noreply.github.com>
    Co-authored-by: Hitenjain14 <57557631+Hitenjain14@users.noreply.github.com>

--- migration/migrate.go
+++ migration/migrate.go
@@ -129,7 +129,7 @@ func InitMigration(mConfig *MigrationConfig) error {
 		zlogger.Logger.Error(err)
 		return err
 	}
-	mConfig.ChunkSize = dStorageService.GetChunkWriteSize()
+	mConfig.ChunkSize = int64(mConfig.ChunkNumber) * dStorageService.GetChunkWriteSize()
 	zlogger.Logger.Info("Getting aws storage service")
 	awsStorageService, err := s3.GetAwsClient(
 		mConfig.Bucket,
@@ -616,7 +616,7 @@ func (m *Migration) processChunkDownload(ctx context.Context, sw *util.StreamWri
 			return
 		default:
 		}
-		data, err := m.awsStore.DownloadToMemory(ctx, downloadObjMeta.ObjectKey, int64(offset), int64(chunkSize))
+		data, err := m.awsStore.DownloadToMemory(ctx, downloadObjMeta.ObjectKey, int64(offset), int64(chunkSize), downloadObjMeta.Size)
 		if err != nil {
 			migrator.DownloadDone(downloadObjMeta, "", err)
 			ctx.Err()
